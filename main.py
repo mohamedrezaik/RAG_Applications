02b5f1637c4c902b903494e1378b7425e96ac286
MohamedRezaik <mohamed.rezaik4@gmail.com>
2024-09-25 13:38:03 +0300

    Updating the app to be more stable

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 
+# Loading all environment variables into the system and make them globally accessable by setting them in main.py
+from dotenv import load_dotenv
+load_dotenv(".env")
+
+from routes import base
+
 # Create FastApi object
 app = FastAPI()
 
-# Create gate function to be accessed by api requests
-@app.get("/dev")
-def welcome():
-    return {
-        "message": "Hello world!"
-    }
+# Adding the base route to our app
+app.include_router(base.base_router)
--- routes/base.py
+++ routes/base.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI, APIRouter
+import os
 
 # Create the default router
-base_router = APIRouter()
+base_router = APIRouter(
+    prefix="/app/v1" # Adding this prefix for all routes Links
+)
 
-# Create the default function responsible for default response
+# Create the default function responsible for default response(app name and version)
 @base_router.get("/")
 def default_fun():
+    # Loading env variables from the system
+    app_name = os.getenv("APP_NAME")
+    app_version = os.getenv("APP_VERSION")
+
     return {
-        "message": "Welcome!"
+        "APP_NAME": app_name,
+        "APP_VERSION": app_version
     }
